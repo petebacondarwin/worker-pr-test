d7fef465af4ed7ab116b6fe70c0e329d9a75da3f
Peter Bacon Darwin <pbacondarwin@cloudflare.com>
2024-02-15 09:25:10 +0000

    TEST

--- .github/scripts/update-deploy-prs.ts
+++ .github/scripts/update-deploy-prs.ts
@@ -1,17 +1,17 @@
 import { Octokit } from "@octokit/action";
+import {} from "octokit";
 import { execSync } from "child_process";
 
 (async () => {
   const octokit = new Octokit();
 
-  const deployWorkerPRs = await getDeployPRs(octokit);
-  console.log(deployWorkerPRs.map((pr) => pr.title));
-
   const deployBranches = await getDeployBranches();
-  console.log(deployBranches);
+  console.log("Found the following worker deploy branches", deployBranches);
+  const deployWorkerPRs = await getDeployPRs(octokit, deployBranches);
+  console.log(deployWorkerPRs.map((pr) => pr.title));
 })();
 
-async function getDeployPRs(octokit: Octokit) {
+async function getDeployPRs(octokit: Octokit, deployBranches: Map<string>) {
   const openPRs = await octokit.pulls.list({
     state: "open",
     owner: "petebacondarwin",
@@ -27,7 +27,11 @@ async function getDeployBranches() {
   const branches = execSync("git branch --list -r", {
     encoding: "utf8",
   });
-  return branches
-    .split("\n")
-    .map((branch) => branch.replace(/origin\/(.*)\s*/g, "$1").trim());
+  return new Map(
+    branches
+      .split("\n")
+      .map((branch) => branch.replace(/origin\/(.*)\s*/g, "$1").trim())
+      .filter((branch) => branch.startsWith("deploy-worker/"))
+      .map((branch) => [branch, null])
+  );
 }
