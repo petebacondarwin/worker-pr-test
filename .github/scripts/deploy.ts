424cee3962ad162a4ea7f64bc1f19f6221db9725
Peter Bacon Darwin <pbacondarwin@cloudflare.com>
2024-02-22 16:19:16 +0000

    add a real worker

--- .github/scripts/deploy.ts
+++ .github/scripts/deploy.ts
@@ -1,2 +1,27 @@
-console.log("Deploying");
-console.log(process.env.PUBLISHED_PACKAGES);
+import { execSync } from "child_process";
+import { resolve } from "path";
+
+const WORKER_PACKAGES = ["package-b"];
+
+type PublishedPackage = { name: string; version: string };
+
+console.log("Checking for Workers to deploy...");
+const packages: PublishedPackage[] = JSON.parse(
+  process.env.PUBLISHED_PACKAGES ?? "[]"
+);
+
+for (const pkg of packages) {
+  if (WORKER_PACKAGES.includes(pkg.name)) {
+    console.log("Deploying", pkg.name, "at", pkg.version);
+    execSync(`pnpm wrangler deploy`, {
+      cwd: resolve(pkg.name),
+      env: {
+        ...process.env,
+        CLOUDFLARE_API_TOKEN: process.env.CLOUDFLARE_API_TOKEN,
+      },
+      stdio: "inherit",
+    });
+  } else {
+    console.log("Skipping", pkg.name, "at", pkg.version);
+  }
+}
