ae7d9ce3844ea42e590c07dc26a0f7b3bc286950
Peter Bacon Darwin <pbacondarwin@cloudflare.com>
2024-02-23 13:26:59 +0000

    tidy up deploy.ts

--- .github/scripts/deploy.ts
+++ .github/scripts/deploy.ts
@@ -1,18 +1,21 @@
 import { execSync } from "child_process";
 import { resolve } from "path";
 
+// This list tells us which packages to deploy as a Worker
+// We could maybe automate this by looking for wrangler.toml files...
 const WORKER_PACKAGES = ["package-b"];
 
 type PublishedPackage = { name: string; version: string };
-
-console.log("Checking for Workers to deploy...");
 const packages: PublishedPackage[] = JSON.parse(
   process.env.PUBLISHED_PACKAGES ?? "[]"
 );
 
+console.log("Checking for Workers to deploy...");
 for (const pkg of packages) {
+  let deployedWorkerCount = 0;
   if (WORKER_PACKAGES.includes(pkg.name)) {
     console.log("Deploying", pkg.name, "at", pkg.version);
+    deployedWorkerCount++;
     execSync(`pnpm wrangler deploy`, {
       cwd: resolve(pkg.name),
       env: {
@@ -23,5 +26,10 @@ for (const pkg of packages) {
     });
   } else {
     console.log("Skipping", pkg.name, "at", pkg.version);
+  }
+  if (deployedWorkerCount === 0) {
+    console.log("No Workers to deploy.");
+  } else {
+    console.log("Deployed", deployedWorkerCount, "workers");
   }
 }
